e4a239cf7a0b4b87dda5e92dd5623b13db861579
gehongtao.1 <gehongtao.1@bytedance.com>
2026-01-22 14:25:01 +0800

    feat: 🎸 drop path

--- src/apps/intlReset/config.ts
+++ src/apps/intlReset/config.ts
@@ -2,11 +2,11 @@ export const config = {
   languageJsonList: [
     {
       language: 'en',
-      jsonPath: '/Users/bytedance/Desktop/fe-monorepo/apps/lite-tms-ui/app/src/lang/en.json',
+      jsonPath: 'src/lang/en.json',
     },
     {
       language: 'id',
-      jsonPath: '/Users/bytedance/Desktop/fe-monorepo/apps/lite-tms-ui/app/src/lang/id.json',
+      jsonPath: 'src/lang/id.json',
     },
   ],
 };
